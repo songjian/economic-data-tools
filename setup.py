2f89ea183ceba5affc7a54c6409c519b47d33972
sj <songjian@codeorder.cn>
2022-04-02 09:52:39 +0800

    更新python包信息

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="经济数据工具",                            # 包的简介描述
     long_description=long_description,                      # 包的详细介绍(一般通过加载README.md)
     long_description_content_type="text/markdown",          # 和上条命令配合使用，声明加载的是markdown文件
-    url="https://github.com/songjian/tzfx",                              # 项目开源地址，我这里写的是同性交友官网，大家可以写自己真实的开源网址
+    url="https://github.com/songjian/economic-data-tools",                              # 项目开源地址，我这里写的是同性交友官网，大家可以写自己真实的开源网址
     packages=setuptools.find_packages(),                    # 如果项目由多个文件组成，我们可以使用find_packages()自动发现所有包和子包，而不是手动列出每个包，在这种情况下，包列表将是example_pkg
     classifiers=[                                           # 关于包的其他元数据(metadata)
         "Programming Language :: Python :: 3",              # 该软件包仅与Python3兼容
